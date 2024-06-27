251ce617b670429e536fa8c48cbd2b75def9f1aa
Tan Shih Jen <e1094875@u.nus.edu>
2024-06-27 10:10:29 +0800

    update code

---
+++ README.md
@@ -0,0 +1,17 @@
+## WikiTable Extractor
+
+### Background
+
+The WikiTable Extractor is designed to simplify the process of extracting tables from Wikipedia pages. By entering the URL of a Wikipedia page, users can quickly and efficiently pull all the tables present on the page into a usable format. This tool leverages the extensive data resources available on Wikipedia, making it a valuable asset for anyone needing to analyze, manipulate, or repurpose structured data.
+
+#### Potential Use Cases
+- Academics and researchers can extract statistical data and other structured information for analysis without manual copying.
+- Data scientists can quickly gather datasets for machine learning, data visualization, and other data-driven projects.
+- Teachers and students can use the extracted tables for assignments, presentations, and educational materials.
+- Businesses can pull relevant market data, financial statistics, and other information to support decision-making processes.
+- Journalists and content creators can quickly obtain data to support articles, reports, and multimedia content.
+- Hobbyists and enthusiasts can use the tool to gather data for personal projects, such as sports statistics, historical records, or hobby-related information.
+
+![app](img/app.png)
+
+<div>⏩ <a href ="https://shihjen-pdf-imageextractor-app-473w1q.streamlit.app/">Web Application</a></div>
--- app.py
+++ app.py
@@ -1,10 +1,12 @@
+# import essential libraries
 import pandas as pd
 import streamlit as st
 from streamlit_lottie import st_lottie
 import requests
 import json
 
-st.set_page_config(page_title='Wikipedia Table Extractor',
+# Streamlit page configuration
+st.set_page_config(page_title='WikiTable Extractor',
                    page_icon = ':wrench:',
                    layout = 'wide',
                    initial_sidebar_state = 'auto')
@@ -15,10 +17,26 @@ def load_lottiefile(filepath: str):
         return json.load(f)
 lottie_cover = load_lottiefile('img/cover.json')    
 
-st.title('Wikipedia Table Extractor')
+# function to convert the extracted table in csv format
+@st.cache_data
+def convert_df(df):
+    # IMPORTANT: Cache the conversion to prevent computation on every rerun
+    return df.to_csv().encode("utf-8")
+
+# application description
+description = '''
+The WikiTable Extractor is designed to simplify the process of extracting tables from Wikipedia pages. 
+By entering the URL of a Wikipedia page, users can quickly and efficiently pull all the tables present on the page into a usable format. 
+This tool leverages the extensive data resources available on Wikipedia, making it a valuable asset for anyone needing to analyze, manipulate, or repurpose structured data.
+'''
+
+
+
 col1, col2 = st.columns([1,3],gap='large')
 
 with col1:
+    st.title('WikiTable Extractor')
+    st.write(f'##### {description}')
     st.lottie(lottie_cover, speed=1, reverse=False, loop=True, quality='low', height=500, key='first_animate')
     container = st.container(border=True)
     container.title('URL Input')
@@ -39,6 +57,13 @@ with col1:
                     for i in range(number_table):
                         st.markdown(f'### Table {i+1}')
                         st.dataframe(df_list[i], use_container_width=True)
+                        csv = convert_df(df_list[i])
+                        st.download_button(
+                            label='Download data as CSV',
+                            data=csv,
+                            file_name=f'table{i+1}.csv',
+                            mime='text/csv',
+                            )
         except:
             st.write('Invalid Input')
 
